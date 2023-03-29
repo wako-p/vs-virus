98840ed91ef219c40eba2bb0cf8c1dccb5a8268b
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-03-29 11:08:49 +0000

    feat(virus): プレイヤーの追加

---
+++ app/src/test/java/virus/PlayerTest.java
@@ -0,0 +1,34 @@
+package virus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+class PlayerTest {
+
+    @Nested
+    class CreateTest {
+
+        @Test
+        void 引数に名前を指定して生成することができる() {
+            // Given:
+            // UUIDのrandomUUID()が固定値を返すようにモックする
+            var fakeUUID = UUID.fromString("00000000-0000-0000-0000-000000000001");
+            var uuidMock = Mockito.mockStatic(UUID.class);
+            uuidMock.when(UUID::randomUUID).thenReturn(fakeUUID);
+
+            // When:
+            var player = Player.create("P1");
+
+            // Then:
+            assertEquals(fakeUUID, player.id);
+            assertEquals("P1", player.name);
+        }
+
+    }
+
+}
