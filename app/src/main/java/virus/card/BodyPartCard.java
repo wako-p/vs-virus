54d8f49215de1c796331ea7259ccda8876e274c4
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-03-30 14:14:30 +0000

    refactor(virus.card): からだパーツカードの変数名の統一

--- app/src/main/java/virus/card/BodyPartCard.java
+++ app/src/main/java/virus/card/BodyPartCard.java
@@ -8,9 +8,9 @@ public final class BodyPartCard implements ICard {
     private final Color color;
     private final Status status;
 
-    private BodyPartCard(final Color color, final Status health) {
+    private BodyPartCard(final Color color, final Status status) {
         this.color = color;
-        this.status = health;
+        this.status = status;
     }
 
     public static BodyPartCard create(final Color color) {
