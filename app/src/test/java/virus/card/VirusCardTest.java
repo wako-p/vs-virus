7342ceb2cc48a273837e8862c46c2e355f4a465b
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-03-30 12:24:50 +0000

    test(virus.card): カードの生成テストをパラメータライズ化

--- app/src/test/java/virus/card/VirusCardTest.java
+++ app/src/test/java/virus/card/VirusCardTest.java
@@ -2,19 +2,38 @@ package virus.card;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class VirusCardTest {
+
     @Nested
     class CreateTest {
-        @Test
-        void 引数に指定した色のウィルスカードを生成できる() {
+
+        static Stream<Arguments> colors() {
+            return Stream.of(
+                Arguments.of(Color.BLUE, Color.BLUE),
+                Arguments.of(Color.RED, Color.RED),
+                Arguments.of(Color.GREEN, Color.GREEN),
+                Arguments.of(Color.YELLOW, Color.YELLOW),
+                Arguments.of(Color.MULTI, Color.MULTI)
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("colors")
+        void 引数に指定した色のウィルスカードを生成できる(Color color, Color expected) {
             // When:
-            var card = VirusCard.create(Color.BLUE);
+            var card = VirusCard.create(color);
 
             // Then:
-            assertEquals(Color.BLUE, card.color());
+            assertEquals(expected, card.color());
         }
+
     }
+
 }
