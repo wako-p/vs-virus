ea0768c9029cb75bbb254be33edebcebd0de1ebf
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-03-31 12:27:54 +0000

    feat(virus.card): からだパーツカードの状態遷移を実装(色による制約は未実装)

--- app/src/main/java/virus/card/BodyPartCard.java
+++ app/src/main/java/virus/card/BodyPartCard.java
@@ -6,7 +6,7 @@ package virus.card;
 public final class BodyPartCard implements ICard {
 
     private final Color color;
-    private final Status status;
+    private Status status;
 
     private BodyPartCard(final Color color, final Status status) {
         this.color = color;
@@ -23,6 +23,44 @@ public final class BodyPartCard implements ICard {
 
     public Status status() {
         return this.status;
+    }
+
+    public void infection(VirusCard virus) {
+
+        if (this.status == Status.HEALTHY) {
+            this.status = Status.INFECTED;
+            return;
+        }
+
+        if (this.status == Status.INFECTED) {
+            this.status = Status.SYMPTOMATIC;
+            return;
+        }
+
+        if (this.status == Status.PASSIVELY_IMMUNIZED) {
+            this.status = Status.HEALTHY;
+            return;
+        }
+
+    }
+
+    public void care(MedicineCard medicine) {
+
+        if (this.status == Status.HEALTHY) {
+            this.status = Status.PASSIVELY_IMMUNIZED;
+            return;
+        }
+
+        if (this.status == Status.PASSIVELY_IMMUNIZED) {
+            this.status = Status.IMMUNIZED;
+            return;
+        }
+
+        if (this.status == Status.INFECTED) {
+            this.status = Status.HEALTHY;
+            return;
+        }
+
     }
 
 }
--- app/src/test/java/virus/card/BodyPartCardTest.java
+++ app/src/test/java/virus/card/BodyPartCardTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -63,4 +64,104 @@ class BodyPartCardTest {
             assertEquals(expected, actual);
         }
     }
+
+    @Nested
+    class InfectionTest {
+        @Test
+        void 健康から感染() {
+            // Given:
+            var virus = VirusCard.create(Color.BLUE);
+            var body = BodyPartCard.create(Color.BLUE);
+
+            // When:
+            body.infection(virus);
+
+            // Then:
+            assertEquals(Status.INFECTED, body.status());
+        }
+
+        @Test
+        void 感染から発症() {
+            // Given:
+            var virus1 = VirusCard.create(Color.BLUE);
+            var virus2 = VirusCard.create(Color.BLUE);
+
+            var body = BodyPartCard.create(Color.BLUE);
+            body.infection(virus1);
+
+            // When:
+            body.infection(virus2);
+
+            // Then:
+            assertEquals(Status.SYMPTOMATIC, body.status());
+        }
+
+        @Test
+        void 仮免疫から健康() {
+            // Given:
+            var medicine = MedicineCard.create(Color.BLUE);
+            var virus = VirusCard.create(Color.BLUE);
+
+            var body = BodyPartCard.create(Color.BLUE);
+            body.care(medicine);
+
+            // When:
+            body.infection(virus);
+
+            // Then:
+            assertEquals(Status.HEALTHY, body.status());
+        }
+
+    }
+
+    @Nested
+    class CareTest {
+
+        @Test
+        void 健康から仮免疫() {
+            // Given:
+            var medicine = MedicineCard.create(Color.BLUE);
+            var body = BodyPartCard.create(Color.BLUE);
+
+            // When:
+            body.care(medicine);
+
+            // Given:
+            assertEquals(Status.PASSIVELY_IMMUNIZED, body.status());
+        }
+
+        @Test
+        void 仮免疫から免疫() {
+            // Given:
+            var medicine1 = MedicineCard.create(Color.BLUE);
+            var medicine2 = MedicineCard.create(Color.BLUE);
+
+            var body = BodyPartCard.create(Color.BLUE);
+            body.care(medicine1);
+
+            // When:
+            body.care(medicine2);
+
+            // Given:
+            assertEquals(Status.IMMUNIZED, body.status());
+        }
+
+        @Test
+        void 感染から健康() {
+            // Given:
+            var virus = VirusCard.create(Color.BLUE);
+            var medicine = MedicineCard.create(Color.BLUE);
+
+            var body = BodyPartCard.create(Color.BLUE);
+            body.infection(virus);
+
+            // When:
+            body.care(medicine);
+
+            // Given:
+            assertEquals(Status.HEALTHY, body.status());
+        }
+
+    }
+
 }
