5d09b787f6bce4e13a6914856de1a30c1dd0f29c
shimokawabe masahiro <o9o76gp381w@gmail.com>
2023-03-31 03:04:46 +0000

    refactor(virus.card): 状態名の変更

--- app/src/main/java/virus/card/Status.java
+++ app/src/main/java/virus/card/Status.java
@@ -4,8 +4,8 @@ package virus.card;
  * 状態
  */
 public enum Status {
-    HEALTHY,    // 健康
-    INFECTED,   // 感染
-    VACCINATED, // ワクチン
-    IMMUNIZED;  // 免疫
+    HEALTHY,                // 健康
+    INFECTED,               // 感染
+    PASSIVELY_IMMUNIZED,    // 仮免駅
+    IMMUNIZED;              // 免疫
 }
