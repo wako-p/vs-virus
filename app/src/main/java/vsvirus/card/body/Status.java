c6b7c6f41c48a1d163a2ed472d69d8f4a38d889f
shimokawabe masahiro <o9o76gp381w@gmail.com>
2024-01-01 04:46:39 +0000

    refactor(vsvirus.card.body): プロパティの追加

--- app/src/main/java/vsvirus/card/body/Status.java
+++ app/src/main/java/vsvirus/card/body/Status.java
@@ -1,18 +1,25 @@
 package vsvirus.card.body;
 
+import lombok.Getter;
 import vsvirus.card.ICard;
 
 /**
  * 状態
  */
 public enum Status {
-    HEALTHY,                // 健康
-    INFECTED,               // 感染
-    SYMPTOMATIC,            // 発症
-    PASSIVELY_IMMUNIZED,    // 仮免駅
-    IMMUNIZED;              // 免疫
+    HEALTHY("健康"),
+    INFECTED("感染"),
+    SYMPTOMATIC("発症"),
+    PASSIVELY_IMMUNIZED("仮免疫"),
+    IMMUNIZED("免疫");
 
+    @Getter
+    private String name;
     private static Trasitions trasitions = new Trasitions();
+
+    private Status(final String name) {
+        this.name = name;
+    }
 
     public Status next(final ICard card) {
         return trasitions.next(this, card.getClass());
